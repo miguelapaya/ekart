2131376f689cbd535a1200b3a0c3c2468f537ed4
Miguel Apaya <90662465+miguelapaya@users.noreply.github.com>
2022-07-17 22:08:24 +0800

    Add files via upload
    first commit

---
+++ cart-services/src/main/java/capstone/ekart/cartservices/CartServicesApplication.java
@@ -0,0 +1,22 @@
+package capstone.ekart.cartservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class CartServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartServicesApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+}
---
+++ payment-services/src/main/java/capstone/ekart/paymentservices/controller/PaymentController.java
@@ -0,0 +1,74 @@
+package capstone.ekart.paymentservices.controller;
+
+import capstone.ekart.paymentservices.entity.Payment;
+import capstone.ekart.paymentservices.model.Cart;
+import capstone.ekart.paymentservices.model.CartPaymentResponse;
+import capstone.ekart.paymentservices.repository.PaymentRepository;
+import capstone.ekart.paymentservices.services.PaymentService;
+import capstone.ekart.paymentservices.services.ValidatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private ValidatorService validatorService;
+
+    @GetMapping("/id/{paymentId}")
+    public ResponseEntity<Object> getPaymentById(@PathVariable Integer paymentId){
+        List<Payment> paymentList = paymentService.getPaymentById(paymentId);
+        return new ResponseEntity<>(paymentList, HttpStatus.OK);
+    }
+
+    /*@PostMapping("/pay-cart/new")
+    public ResponseEntity<?> createPayment(@RequestBody Payment payment){
+        return new ResponseEntity<>(paymentService.savePayment(payment), HttpStatus.OK);
+    }*/
+
+
+    @PostMapping("/pay-cart/new")
+    public ResponseEntity<?> createPayment(@Valid @RequestBody Payment payment, BindingResult bindingResult){
+
+        Map<String, String> errorMap = validatorService.paymentValidation(bindingResult);
+        if(errorMap.isEmpty())
+            return new ResponseEntity<>(paymentService.savePayment(payment), HttpStatus.CREATED);
+        else {
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @GetMapping("/payment-details/id/{paymentId}")
+    public ResponseEntity<CartPaymentResponse> getCartPaymentDetails(@PathVariable Integer paymentId){
+
+        CartPaymentResponse cartPaymentResponse = new CartPaymentResponse();
+        Payment payment = paymentRepository.findById(paymentId).get();
+        cartPaymentResponse.setPayment(payment);
+
+        List<Cart> paymentList = restTemplate.getForObject("http://localhost:9002/my-cart/all-carts/cart-id/" + paymentId , List.class);
+        cartPaymentResponse.setCartList(paymentList);
+
+        return new ResponseEntity<>(cartPaymentResponse, HttpStatus.OK);
+    }
+
+
+}
